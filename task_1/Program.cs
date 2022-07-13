6a92faecee44cba6dd02957945963ac755f6f549
Nadiya <nadiyanzmp@gmail.com>
2022-07-13 15:54:47 +0400

    homework 1 - Programming

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите любое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите любое число");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("максимальное число = "+ num1 );
+    Console.WriteLine("минимальное число = "+ num2 );
+}
+else 
+{
+    Console.WriteLine("максимальное число = "+ num2 );
+    Console.WriteLine("минимальное число = "+ num1 );
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("введите число ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число");
+int C = int.Parse(Console.ReadLine());
+
+if (A > B && A > C)
+{Console.WriteLine("MAX number = " + A);}
+
+else if (B > C) {Console.WriteLine("MAX number = " + B);}
+
+else {Console.WriteLine("MAX number = " + C);}
