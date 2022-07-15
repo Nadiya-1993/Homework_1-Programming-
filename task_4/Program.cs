5318676417cd7027c551bb557354892420b5c408
Nadiya <nadiyanzmp@gmail.com>
2022-07-15 21:09:50 +0400

    Homework - task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите длину: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= n; i += 2)
+    Console.WriteLine(i);
